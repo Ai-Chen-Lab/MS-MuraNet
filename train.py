67d1d69fb52433cabaf941e1c0e4d0822c8de6bc
Ai-Chen-Lab <chen03810@gmail.com>
2026-03-03 16:56:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -30,10 +30,12 @@ args = {
     'batch_size': 8,
     'lr': 0.001,
     'workers': 5,
-    'tra_img_dir': '',            
+    'tra_img_dir': '',              
+    'tra_the_dir': '',
+    'tra_his_dir': '',
     'tra_lbl_dir': '',     
-    'image_ext': '',
-    'label_ext': '',
+    'image_ext': '.jpg',
+    'label_ext': '.png',
     'checkpoint': '',
 }
 
@@ -44,38 +46,92 @@ bce_loss = nn.BCELoss(reduction='mean')
 ssim_loss = pytorch_ssim.SSIM(window_size=11, size_average=True)
 iou_loss = pytorch_iou.IOU(size_average=True)
 
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+    
+    def forward(self, pred, target):
+        smooth = 1e-5
+        pred = pred.view(-1)
+        target = target.view(-1)
+        intersection = (pred * target).sum()
+        dice = (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
+        return 1. - dice
+
+class GradientLoss(nn.Module):
+    def __init__(self):
+        super(GradientLoss, self).__init__()
+    
+    def forward(self, pred, target):
+        def gradient(x):
+            h_x = x.size()[2]
+            w_x = x.size()[3]
+            
+            # 计算水平梯度
+            gradient_x = torch.abs(x[:, :, :, :-1] - x[:, :, :, 1:])
+            # 计算垂直梯度
+            gradient_y = torch.abs(x[:, :, :-1, :] - x[:, :, 1:, :])
+            
+            # 对称填充以保持尺寸一致
+            gradient_x = F.pad(gradient_x, (0, 1, 0, 0), mode='replicate')
+            gradient_y = F.pad(gradient_y, (0, 0, 0, 1), mode='replicate')
+            
+            gradient = torch.cat([gradient_x, gradient_y], dim=1)
+            return gradient
+        
+        pred_grad = gradient(pred)
+        target_grad = gradient(target)
+        
+        # 确保尺寸一致
+        if pred_grad.size() != target_grad.size():
+            target_grad = F.interpolate(target_grad, size=pred_grad.size()[2:], mode='bilinear', align_corners=True)
+        
+        loss = F.l1_loss(pred_grad, target_grad)
+        return loss
+    
+bce_loss = nn.BCELoss(reduction='mean')
+ssim_loss = pytorch_ssim.SSIM(window_size=11, size_average=True)
+iou_loss = pytorch_iou.IOU(size_average=True)
+gradient_loss = GradientLoss()
+
 def train_loss(pred, target):
     bce_out = bce_loss(pred, target)
     ssim_out = 1 - ssim_loss(pred, target)
     iou_out = iou_loss(pred, target)
-    loss = bce_out + ssim_out + iou_out
+    gradient_out = gradient_loss(pred, target)
+    loss = bce_out + ssim_out + iou_out + 0.5 * gradient_out
     return loss
 
-def muti_loss_fusion(s_out, s0, s1, s2, s3, s4, sb, labels_v):
+def muti_loss_fusion(s_out, s0, s1, s2, s3, sb, labels_v):
     loss0 = train_loss(s_out, labels_v)
     loss1 = train_loss(s0, labels_v)
     loss2 = train_loss(s1, labels_v)
     loss3 = train_loss(s2, labels_v)
     loss4 = train_loss(s3, labels_v)
-    loss5 = train_loss(s4, labels_v)
-    loss6 = train_loss(sb, labels_v)
-    loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+    loss5 = train_loss(sb, labels_v)
+    loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5
     return loss0, loss
 
 def collate_fn(batch):
     for item in batch:
         item['image'] = item['image'].float()
+        item['thermal'] = item['thermal'].float()
+        item['hist'] = item['hist'].float()
         item['label'] = item['label'].float()
     return {
         'image': torch.stack([b['image'] for b in batch]),
+        'thermal': torch.stack([b['thermal'] for b in batch]),
+        'hist': torch.stack([b['hist'] for b in batch]),
         'label': torch.stack([b['label'] for b in batch]),
     }
 
 def main():
-    logging.info(args)
+    logging.info(args)  
     print(args)
 
     tra_img_name_list = glob.glob(os.path.join(args['tra_img_dir'], '*' + args['image_ext']))
+    tra_his_name_list = glob.glob(os.path.join(args['tra_his_dir'], '*' + args['image_ext']))
+    tra_the_name_list = glob.glob(os.path.join(args['tra_the_dir'], '*' + args['image_ext']))
     tra_lbl_name_list = [os.path.join(args['tra_lbl_dir'], os.path.splitext(os.path.basename(img))[0] + args['label_ext'])
                          for img in tra_img_name_list]
     logging.info('**********************************************')
@@ -90,14 +146,17 @@ def main():
 
     salobj_dataset = SalObjDataset(
         img_name_list=tra_img_name_list,
+        tra_the_dir=tra_the_name_list,
+        tra_his_dir=tra_his_name_list,
         lbl_name_list=tra_lbl_name_list,
         transform=transforms.Compose([
             RescaleT(256),
-            RandomCrop(224),
+            RandomCrop(256),
             ToTensorLab(flag=0)
         ])
     )
-
+    # sample = next(iter(salobj_dataset))
+    # print("Keys in data sample:", sample.keys())
     salobj_dataloader = DataLoader(
         salobj_dataset,
         batch_size=args['batch_size'],
@@ -109,6 +168,7 @@ def main():
     net = Net(in_channels=3).cuda()
 
     optimizer = optim.RMSprop(net.parameters(), lr=args['lr'], alpha=0.9)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=110, gamma=0.1)
 
     ite_num = 0
     running_loss = 0.0
@@ -120,14 +180,16 @@ def main():
         for i, data in enumerate(salobj_dataloader):
             ite_num += 1
             ite_num4val += 1
-            inputs_v, labels_v = data['image'], data['label']
-            inputs_v = inputs_v.cuda()
-            labels_v = labels_v.cuda()
+            images = data['image'].cuda()
+            thermals = data['thermal'].cuda()
+            hist = data['hist'].cuda()
+            labels = data['label'].cuda()
 
             optimizer.zero_grad()
 
-            s_out, s0, s1, s2, s3, s4, sb = net(inputs_v)
-            loss2, loss = muti_loss_fusion(s_out, s0, s1, s2, s3, s4, sb, labels_v)
+            s_out, s0, s1, s2, s3, s4, sb = net(images, thermals, hist)
+            loss2, loss = muti_loss_fusion(s_out, s0, s1, s2, s3, s4, sb, labels)
+
             loss.backward()
             optimizer.step()
 
@@ -141,7 +203,9 @@ def main():
                 epoch + 1, args['epoch'], (i + 1) * args['batch_size'], len(tra_img_name_list), ite_num,
                 running_loss / ite_num4val, running_tar_loss / ite_num4val))
 
-        if (epoch + 1) % 20 == 0:
+        scheduler.step()
+
+        if (epoch + 1) % 1 == 0:
             torch.save(net.state_dict(), os.path.join(
                 args['checkpoint'],
                 f"Net_epoch_{epoch + 1}_trnloss_{running_loss / ite_num4val:.4f}_priloss_{running_tar_loss / ite_num4val:.4f}.pth"))
